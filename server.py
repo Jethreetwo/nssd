3f0f813ed680c941f4ec4590200320467cd093fb
Jethreetwo <41649577+Jethreetwo@users.noreply.github.com>
2025-06-05 20:37:33 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -37,4 +37,4 @@ def submit():
     return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=False)
